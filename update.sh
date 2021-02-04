27d643f112c28d3a0ce44a4596d515e77552a7ef
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-02-04 16:51:23 +0100

    :art: Remove empty line between items
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- update.sh
+++ update.sh
@@ -108,7 +108,7 @@ for latest in "${latests[@]}"; do
 			echo "${DOCKER_TAGS} " > "$dir/.dockertags"
 
 			# Add README tags
-			readmeTags="$readmeTags\n-   ${DOCKER_TAGS} (\`$dir/Dockerfile\`)\n"
+			readmeTags="$readmeTags\n-   ${DOCKER_TAGS} (\`$dir/Dockerfile\`)"
 
 			# Add Travis-CI env var
 			travisEnv='\n    - VERSION='"$version"' VARIANT='"$variant$travisEnv"
@@ -125,7 +125,7 @@ done
 
 # update README.md
 sed '/^<!-- >Docker Tags -->/,/^<!-- <Docker Tags -->/{/^<!-- >Docker Tags -->/!{/^<!-- <Docker Tags -->/!d}}' README.md > README.md.tmp
-sed -e "s|<!-- >Docker Tags -->|<!-- >Docker Tags -->\n$readmeTags|g" README.md.tmp > README.md
+sed -e "s|<!-- >Docker Tags -->|<!-- >Docker Tags -->\n$readmeTags\n|g" README.md.tmp > README.md
 rm README.md.tmp
 
 # update .travis.yml
